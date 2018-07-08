2acd53b039e8b7ce694ee73bf6ecf3fb736a8e60
mike-carbone <mfcbone@gmail.com>
2018-07-08 15:53:34 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//Getting all paths from the svg
+const paths = document.getElementsByTagName('path');
+const colorPicker = document.getElementById('color-js');
+let chosenColor = colorPicker.value;
+
+//Adding event listener to each path that will change its color on pick
+for (const value of paths){
+	value.addEventListener("click", function(){
+		this.style.fill = chosenColor;
+		updateCanvas();
+	});
+}
+
+//Updates the chosen color when it is changed
+colorPicker.addEventListener("change", function(){
+	chosenColor = colorPicker.value;
+})
+
+//Updates the canvas when a color is changed so the img href is always up to date
+document.addEventListener("DOMContentLoaded", function(){
+	updateCanvas();
+})
+
+function updateCanvas(){
+	html2canvas(document.getElementsByClassName('svg')[0]).then(function(canvas) {
+    document.body.appendChild(canvas);})
+
+    //Gives the canvas time to initialize before setting href
+	//This usually takes between 100-180ms
+    setTimeout(function(){
+    	let canvas = document.getElementsByTagName('canvas')[0];
+	 	const dl = document.getElementById('dl');
+	 	dl.href = canvas.toDataURL("image/png");
+
+	 	//Removes duplicate canvases so the first one is always up to date
+	    if (canvas){
+	    	canvas.remove();
+	    }
+	}, 250);
+}
+
+
+
