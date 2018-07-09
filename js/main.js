18f6481b2012a1043f13aa2169c7898616817549
mike-carbone <mfcbone@gmail.com>
2018-07-08 22:44:05 -0400

    added/edited comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 //Getting all paths from the svg
-const paths = document.getElementsByTagName('path');
+const paths = document.getElementsByTagName("path");
 
-const colorPicker = document.getElementById('color-js');
+const colorPicker = document.getElementById("color-js");
 let chosenColor = colorPicker.value;
+
+//Updates the chosen color when it is changed through the picker
+colorPicker.addEventListener("change", function(){
+	chosenColor = colorPicker.value;
+});
 
 //Adding event listener to each path that will change the paths color on pick
 for (const value of paths){
@@ -12,29 +17,20 @@ for (const value of paths){
 	});
 }
 
-//Updates the chosen color when it is changed through the picker
-colorPicker.addEventListener("change", function(){
-	chosenColor = colorPicker.value;
-})
-
-//Prepares the download link after site is loaded
-document.addEventListener("DOMContentLoaded", function(){
-	updateCanvas();
-})
-
-//Updates the canvas when a color is changed so the img href is always up to date
+//Updates the canvas when a color is changed
+//makes sure the img href is always up to date
 function updateCanvas(){
 	
 	//Turns the svg into canvas, allows export to png. Display: none in css
-	html2canvas(document.getElementsByClassName('svg')[0]).then(function(canvas){
+	html2canvas(document.getElementsByClassName("svg")[0]).then(function(canvas){
 		document.body.appendChild(canvas);
 	});
 
     //Gives the canvas time to initialize before setting href
     //This usually takes between 100-180ms
     setTimeout(function(){
-    	let canvas = document.getElementsByTagName('canvas')[0];
-	 	const dl = document.getElementById('dl');
+    	let canvas = document.getElementsByTagName("canvas")[0];
+	 	const dl = document.getElementById("dl");
 
 	 	//Sets the download link to the updated img
 	 	dl.href = canvas.toDataURL("image/png");
@@ -48,5 +44,9 @@ function updateCanvas(){
 	return dl;
 }
 
+//Prepares the download link after site is loaded
+document.addEventListener("DOMContentLoaded", function(){
+	updateCanvas();
+});
 
 
