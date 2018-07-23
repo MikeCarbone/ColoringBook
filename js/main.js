bd64e0c02642223a581b2777d2c620f28e4119d7
mike-carbone <mfcbone@gmail.com>
2018-07-23 16:11:26 -0400

    :sparkles: added functionality, color picking

--- js/main.js
+++ js/main.js
@@ -1,77 +1,123 @@
 //Getting all paths from the svg
 const paths = document.getElementsByTagName("path");
+let chosenColor;
 
-$(function() {
-    $('img').click(function(e) {
-        
-        //drawing the custom color wheel
-        if(!this.canvas) {
-            this.canvas = $('<canvas/>')[0];
-            this.canvas.width = this.width;
-            this.canvas.height = this.height;
-            this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
-        }
+function instantiateWheel(){
+    //Makes sure canvas for the color wheel is instantiated
+    var colorWheelCanvasInstantiation = new Promise(function(resolve, reject){
+        html2canvas(document.getElementById("colorWheelImg")).then(function(canvas){
+            document.body.appendChild(canvas);
+            canvas.id = "color-wheel-canvas";
 
-        //getting the pixel data
-        var imgData = this.canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
-        
-        //r,g,b,a values
-        var x = imgData[0];
-        var y = imgData[1];
-        var z = imgData[2];
-        var a = imgData[3];
+            return wasCanvasInstantiated(canvas);
+    });
 
-        $('#output').html('R: ' + x + '<br>G: ' + y + '<br>B: ' + z + '<br>A: ' + a);
+    function wasCanvasInstantiated(canvas){
+        if (canvas){
+                resolve('Color wheel canvas instantiated!');
+            } else {
+                reject(Error('Color wheel canvas did not instantiate. Try reloading the page.'));
+            }
+        }  
+    });
 
-        function colorDeclartion() {
-            var total = 'rgba(' + x + ', ' + y + ', ' + z + ', ' + a + ')';
-            return total;
-        }
+    //What to do after color wheel canvas is instantiated
+    colorWheelCanvasInstantiation.then(function(result) {
+        console.log(result);
+        document.getElementById("colorWheelImg").style.display = "none";
+        document.getElementById("color-wheel-canvas").addEventListener("click", function(e){
+            colorPick(e);
+        });
 
-        //shows the color swatch
-        $('.displayColor').css('background-color', colorDeclartion());
-        chosenColor = $('.displayColor').css('background-color');
-
-        //updates the picture with the chosen color
-        for (const value of paths){
-            value.addEventListener("click", function(){
-            this.style.fill = chosenColor;
-        // updateCanvas();
+    }, function(err){
+        console.log(err);
     });
 }
-    });
-});
 
+function colorPick(e){
+    
+    //Converts the RGB data to hex from the page data
+    function rgbToHex(r, g, b) {
+        if (r > 255 || g > 255 || b > 255)
+            throw "Invalid color component";
+        return ((r << 16) | (g << 8) | b).toString(16);
+    }
+
+    var colorWheelCanvas = document.getElementById("color-wheel-canvas");
+    var context = colorWheelCanvas.getContext('2d');
+   
+   //Gets the position of the area clicked on the canvas
+    if (e.pageX || e.pageY) { 
+      x = e.pageX;
+      y = e.pageY;
+    } else { 
+      x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+      y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+    }
+
+    x -= colorWheelCanvas.offsetLeft;
+    y -= colorWheelCanvas.offsetTop;
+    
+    //Gets image data on the position clicked
+    var p = context.getImageData(x, y, 1, 1).data;
+    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+    
+    //Sets the chosen color to the color clicked
+    chosenColor = hex;
+    console.log(`Chosen color: ${chosenColor} at X: ${x}, Y: ${y}`);
+}
+
+//Listens for a click on each path
+for (const value of paths){
+    value.addEventListener("click", function(){
+        this.style.fill = chosenColor;
+         updateCanvas();
+    });
+}
 
 //Updates the canvas when a color is changed
 //makes sure the img href is always up to date
-
 function updateCanvas(){
+    console.log('updating');
     
-    //Turns the svg into canvas, allows export to png. Display: none in css
+    var imgCanvas = new Promise(function(resolve, reject){
     html2canvas(document.getElementsByClassName("svg")[0]).then(function(canvas){
         document.body.appendChild(canvas);
+        canvas.classList.add('canvas-history');
+
+        return wasCanvasInstantiated(canvas);
+    });
+    
+    function wasCanvasInstantiated(canvas){
+        if (canvas){
+                resolve('Sketch canvas instantiated!');
+            } else {
+                reject(Error('Sketch canvas did not instantiate. Try reloading the page.'));
+            }
+        }  
     });
 
-    //Gives the canvas time to initialize before setting href
-    //This usually takes between 100-180ms
-    setTimeout(function(){
-        let canvas = document.getElementsByTagName("canvas")[0];
+    imgCanvas.then(function(result) {
+        console.log(result);
+
+        let canvas = document.getElementsByClassName("canvas-history")[0];
         const dl = document.getElementById("dl");
 
         //Sets the download link to the updated img
-        dl.href = canvas.toDataURL();
+        dl.href = canvas.toDataURL("image/png");
 
         //Removes duplicate canvases so the first one is always up to date
         if (canvas){
             canvas.remove();
         }
-    }, 250);
 
-    return dl;
+    }, function(err){
+        console.log(err);
+    });
 }
 
 //Prepares the download link after site is loaded
 document.addEventListener("DOMContentLoaded", function(){
     updateCanvas();
+    instantiateWheel();
 });
